5599d32d326c892ab399b4db7c1895b6b8b77c55
christineorlino <christineorlinio@gmail.com>
2019-05-17 00:30:41 -0700

    Update

---
+++ p5/sketch.js
@@ -0,0 +1,68 @@
+function setup() {
+  createCanvas(500, 500);
+    background(118, 211, 196);
+}
+
+function draw() {
+
+  noStroke();
+  
+  fill(255);
+  ellipse(250,250, 200, 200);
+  fill(255);
+  ellipse(150,150, 100, 100);
+  fill(255);
+  ellipse(150, 100, 50, 50);
+  fill(255);
+  ellipse(350, 350, 100, 100);
+  fill(255);
+  ellipse(200, 350, 70, 70);
+  fill(255);
+  ellipse(400, 150, 50, 50);
+  fill(255);
+  ellipse(350, 50, 40, 40);
+  fill(255);
+  ellipse(75, 50, 30, 30);
+  fill(255);
+  ellipse(350, 50, 40, 40);
+  fill(255);
+  ellipse(30, 450, 20, 20);
+  fill(255);
+  ellipse(375, 400, 20, 20);
+  fill(255);
+  ellipse(30, 20, 20, 20);
+  fill(255);
+  ellipse(325, 450, 20, 20);
+  fill(255);
+  ellipse(325, 45, 20, 20);
+  fill(255);
+  ellipse(450, 250, 50, 50);
+  fill(255);
+  ellipse(470, 230, 30, 30);
+  fill(255);
+  ellipse(40, 325, 50, 50);
+  fill(255);
+  ellipse(70, 340, 25, 25);
+  fill(255);
+  ellipse(150, 25, 20, 20);
+  fill(255);
+  ellipse(200, 450, 50, 50);
+  fill(255);
+  ellipse(450, 425, 30, 30);
+  fill(255);
+  ellipse(50, 200, 30, 30);
+  fill(255);
+  ellipse(450, 100, 30, 30);
+  fill(255);
+  ellipse(300, 150, 70, 70);
+  fill(255);
+  ellipse(140, 300, 60, 60);
+  
+  if (mouseIsPressed) {
+  fill(255);
+  } else {
+  fill(255, 255, 255, 4);
+  }
+  ellipse(mouseX, mouseY, 50, 50);
+  
+}
